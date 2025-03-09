a4ac5f53c3ba6ef3fb4aab28ec328213eb1e434e
Ahmad-Alhafne <ahmad4732916@gmail.com>
2025-03-09 16:15:26 +0300

    try to reduce canvas Tech

--- src/constants/index.js
+++ src/constants/index.js
@@ -60,18 +60,7 @@ const services = [
 ];
 
 const technologies = [
-  {
-    name: "HTML 5",
-    icon: html,
-  },
-  {
-    name: "CSS 3",
-    icon: css,
-  },
-  {
-    name: "JavaScript",
-    icon: javascript,
-  },
+
   {
     name: "TypeScript",
     icon: typescript,
@@ -80,18 +69,7 @@ const technologies = [
     name: "React JS",
     icon: reactjs,
   },
-  {
-    name: "Redux Toolkit",
-    icon: redux,
-  },
-  {
-    name: "Tailwind CSS",
-    icon: tailwind,
-  },
-  {
-    name: "Node JS",
-    icon: nodejs,
-  },
+
   {
     name: "Laravel",
     icon: laravel,
@@ -100,18 +78,7 @@ const technologies = [
     name: "Three JS",
     icon: threejs,
   },
-  {
-    name: "git",
-    icon: git,
-  },
-  {
-    name: "figma",
-    icon: figma,
-  },
-  {
-    name: "docker",
-    icon: docker,
-  },
+
 ];
 
 const experiences = [
