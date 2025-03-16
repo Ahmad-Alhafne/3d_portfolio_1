4c081704b63a08d55c82f6de4255d147893f95cb
Ahmad-Alhafne <ahmad4732916@gmail.com>
2025-03-16 11:34:44 +0300

    2 fixing some issus again

---
+++ src/assets/index.js
@@ -0,0 +1,55 @@
+import logo from "./logo.svg";
+import backend from "./backend.webp";
+import creator from "./creator.webp";
+import mobile from "./mobile.webp";
+import web from "./web.webp";
+import github from "./github.webp";
+import eyeicon from "./eyeicon.webp";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+
+import laravel from "./tech/laravel.webp";
+import reactjs from "./tech/reactjs.webp";
+import typescript from "./tech/typescript.webp";
+
+
+import meta from "./company/meta.webp";
+import shopify from "./company/shopify.webp";
+import starbucks from "./company/starbucks.webp";
+import tesla from "./company/tesla.webp";
+
+import iphone from "./iphone.webp";
+import landingpage from "./landingpage.webp";
+import dashboard from "./dashboard.webp";
+import template3 from "./template3.png";
+import template2 from "./template2.png";
+import bondi from "./bondi.png";
+import coca_cola from "./coca_cola.png";
+
+
+export {
+  logo,
+  eyeicon,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  laravel,
+  reactjs,
+  typescript,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  iphone,
+  landingpage,
+  dashboard,
+  template3,
+  template2,
+  bondi,
+  coca_cola,
+};
